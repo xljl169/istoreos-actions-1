8348efc0a87e27570d02d9593e3a74351bbf22d5
xljl169 <lihuizhong620@gmail.com>
2024-03-18 23:22:14 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -11,13 +11,13 @@
 #
 
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.119.1/g' package/base-files/files/bin/config_generate
 
 # 修改子网掩码
-#sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
+#sed -i 's/255.255.255.0/255.255.255.0/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
-# sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/LYY/g' package/base-files/files/bin/config_generate
 
 # ttyd自动登录
 # sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
